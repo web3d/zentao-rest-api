81dfc643880de0e3baacaadb598cb034206e8809
web3d <web3d@live.com>
2015-11-30 20:16:37 +0800

    将 route 抽取到配置文件中

--- data/config/routes.php
+++ data/config/routes.php
@@ -3,9 +3,32 @@
 //更多使用方法请阅读文档:
 //http://docs.slimframework.com/#Routing-Overview
 
-$app->get('/', 'HomeController:index');
-
-$app->group('/api', function() use ($app){
-    $app->get('/articles', 'ArticleController:index');
-    $app->get('/articles/:id', 'ArticleController:show');
-});
+return array(
+    'get' => array(
+        '/users/current.:format' => 'User:fetchByKey',
+        '/users.:format' => 'User:index',
+        '/roles/:id.:format' => 'Role:fetch',
+        '/roles.:format' => 'Role:fetchAll',
+        '/issue_statuses.:format' => 'IssueStatus:index',
+        '/issues/:id.:format' => 'Issue:fetch',
+        '/issues.:format' => 'Issue:fetchAll',
+        '/projects/:id/memberships.:format' => 'ProjectMembership:fetchAll',
+        '/projects/:id/versions.:format' => 'ProjectVersion:fetchAll',
+        '/projects/:id/issue_categories.:format' => 'IssueCategory:fetchAllByProjectId',
+        '/projects/:id.:format' => 'Project:fetch',
+        '/projects.:format' => 'Project:fetchAll',
+        '/enumerations/issue_priorities.:format' => 'Enum:issue_priorities',
+        '/trackers.:format' => 'Tracker:index',
+        '/queries.:format' => 'Query:index',
+        '/' => 'Main:index',
+    ),
+    'post' => array(
+        
+    ),
+    'put' => array(
+        
+    ),
+    'delete' => array(
+        
+    )
+);
--- index.php
+++ index.php
@@ -26,20 +26,14 @@ $app = \zentao\core\Application::app(dirname(ZTNB_ROOT)); //禅道的router
 
 $slim = new \Slim\Slim();
 
-$slim->get('/users/current.:format', '\\zentao\\nb\\resource\User:fetchByKey');
-$slim->get('/users.:format', '\\zentao\\nb\\resource\User:index');
-$slim->get('/roles/:id.:format', '\\zentao\\nb\\resource\Role:fetch');
-$slim->get('/roles.:format', '\\zentao\\nb\\resource\Role:fetchAll');
-$slim->get('/issue_statuses.:format', '\\zentao\\nb\\resource\IssueStatus:index');
-$slim->get('/issues/:id.:format', '\\zentao\\nb\\resource\Issue:fetch');
-$slim->get('/issues.:format', '\\zentao\\nb\\resource\Issue:fetchAll');
-$slim->get('/projects/:id/memberships.:format', '\\zentao\\nb\\resource\ProjectMembership:fetchAll');
-$slim->get('/projects/:id/versions.:format', '\\zentao\\nb\\resource\ProjectVersion:fetchAll');
-$slim->get('/projects/:id/issue_categories.:format', '\\zentao\\nb\\resource\IssueCategory:fetchAllByProjectId');
-$slim->get('/projects/:id.:format', '\\zentao\\nb\\resource\Project:fetch');
-$slim->get('/projects.:format', '\\zentao\\nb\\resource\Project:fetchAll');
-$slim->get('/enumerations/issue_priorities.:format', '\\zentao\\nb\\resource\Enum:issue_priorities');
-$slim->get('/trackers.:format', '\\zentao\\nb\\resource\Tracker:index');
-$slim->get('/queries.:format', '\\zentao\\nb\\resource\Query:index');
-$slim->get('/', '\\zentao\\nb\\resource\Main:index');
+$routes = require __DIR__ . '/data/config/routes.php';
+
+foreach ($routes as $method => $_routes) {
+    if ($_routes) {
+        foreach ($_routes as $rule => $map) {
+            $slim->$method($rule, '\\zentao\\nb\\resource\\' . $map);
+        }
+    }
+}
+
 $slim->run();
